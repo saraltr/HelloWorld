21314d499c8e59fb1b7079814262867b69766304
sara latorre <saralatorre97@gmail.com>
2025-07-05 12:05:17 +0200

    switched to a minimal web server

--- Program.cs
+++ Program.cs
@@ -1,10 +1,59 @@
-﻿// hello world module
-using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.FileProviders;
+using System.Text;
 
-class Program
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+// list of greetings
+string[] greetings = new[]
 {
-    static void Main(string[] args)
+    "Hello World!",
+    "Welcome!",
+    "Greetings!",
+    "Hi there!",
+    "Bonjour, le monde!",
+    "¡Hola, Mundo!",
+};
+
+// static variable to track last index
+int? lastIndex = null;
+
+app.MapGet("/", () =>
+{
+    var random = new Random();
+    int newIndex;
+
+    // ensure different greeting than last
+    do
     {
-        Console.WriteLine("Hello World!");
-    }
-}
+        newIndex = random.Next(greetings.Length);
+    } while (lastIndex.HasValue && newIndex == lastIndex.Value);
+
+    lastIndex = newIndex; // update last used
+
+    var greeting = greetings[newIndex];
+
+    var html = $@"
+        <!DOCTYPE html>
+        <html lang='en'>
+        <head>
+            <meta charset='UTF-8'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <title>Hello App</title>
+            <link rel='stylesheet' href='/styles.css'>
+        </head>
+        <body>
+            <div class='container'>
+                <h1>{greeting}</h1>
+            </div>
+        </body>
+        </html>
+    ";
+
+    return Results.Content(html, "text/html", Encoding.UTF8);
+});
+
+app.Run();
--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # Overview
 
-Hello World simple module
+Hello World simple module that displays a randomly selected greeting message
 
 [Software Demo Video](http://youtube.link.goes.here)
 
 # Development Environment
 
 - **.NET SDK**: .NET 9.0
-- **C#**
+- **Language**: C#
+- **Framework**: ASP.NET Core Minimal API
+- **Run the Project**: dotnet run
 
 # Useful Websites
 
 * [.NET Official Documentation](https://learn.microsoft.com/en-us/dotnet/)
+* [Minimal API with ASP.NET Core](https://learn.microsoft.com/en-us/aspnet/core/tutorials/min-web-api?view=aspnetcore-9.0&tabs=visual-studio)
 * [How to use .NET CLI](https://learn.microsoft.com/en-us/dotnet/core/tools/)
